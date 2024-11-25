7b5c6381dafed5045fdfc9f10aec36cc150b344a
Rafael Manfrim <rafaelmanfrim2004@gmail.com>
2024-11-25 11:55:06 -0300

    Update main.py prod

--- main.py
+++ main.py
@@ -11,10 +11,5 @@ app.add_url_rule('/fetch_colleges/<uf>/<city>', 'fetch_colleges', fetch_colleges
 app.add_url_rule('/fetch_courses/<college>/<campus>', 'fetch_courses', fetch_courses, methods=['GET'])
 app.add_url_rule('/fetch_course/<course_id>', 'fetch_course', fetch_course, methods=['GET'])
 
-
-def main():
-    app.run(debug=True)
-
-
 if __name__ == '__main__':
-    main()
+    app.run(debug=False)
